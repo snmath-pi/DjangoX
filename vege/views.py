afc83b9b9d8483238a26b6693786cec86018e062
Saksham <sakshamnegi40069@gmail.com>
2024-06-27 09:09:47 +0530

    completed project receipe app

--- vege/views.py
+++ vege/views.py
@@ -18,6 +18,10 @@ def receipes(request):
         return redirect('/receipes/')
     
     queryset = Receipe.objects.all()
+    
+    if request.GET.get('search'):
+        queryset = queryset.filter(receipe_name__icontains=request.GET.get('search'))
+        
     context = {'receipes': queryset}
     return render(request, 'receipes.html', context)
 
@@ -29,6 +33,19 @@ def delete_receipe(request, id):
 
 def update_receipe(request, id):
     queryset = Receipe.objects.get(id=id)
+    
+    if request.method== "POST":
+        data = request.POST
+        receipe_name = data.get('receipe_name')
+        receipe_discription = data.get('receipe_discription')
+        receipe_image = request.FILES.get('receipe_image')
+        queryset.receipe_name = receipe_name
+        queryset.receipe_discription = receipe_discription
+        if receipe_image:
+            queryset.receipe_image = receipe_image
+        queryset.save()
+        return redirect('/receipes/')
+    
     context = {'receipe': queryset}
     return render(request, 'update_receipes.html', context)
     
